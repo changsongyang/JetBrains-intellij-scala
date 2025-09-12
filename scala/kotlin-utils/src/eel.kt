158cccbed47112c6b0c2fc9ef41b832fb7b04e28
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-09-12 10:23:32 +0200

    [eel, sbt] The sbt import process is not cancellable in all usages and we need to comply with this for now
    - Make all paths absolute before using eel.

--- scala/kotlin-utils/src/eel.kt
+++ scala/kotlin-utils/src/eel.kt
@@ -1,31 +1,34 @@
 @file:JvmName("EelUtil")
+@file:Suppress("UnstableApiUsage")
 
 package org.jetbrains.plugins.scala.kotlin.util
 
-import com.intellij.openapi.progress.runBlockingCancellable
+import com.intellij.openapi.progress.runBlockingMaybeCancellable
 import com.intellij.platform.eel.provider.asEelPath
 import com.intellij.platform.eel.provider.getEelDescriptor
 import com.intellij.platform.eel.spawnProcess
 import com.intellij.util.containers.tail
+import io.ktor.util.normalizeAndRelativize
 import java.io.File
 
-fun runProcess(exec: File, workingDirectory: File, passParentEnvironment: Boolean, environment: Map<String, String>, commands: List<String>): Process = runBlockingCancellable {
-  val execPath = exec.toPath()
-  val eelDescriptor = execPath.getEelDescriptor()
-  val eel = eelDescriptor.toEelApi()
+fun runProcess(exec: File, workingDirectory: File, passParentEnvironment: Boolean, environment: Map<String, String>, commands: List<String>): Process =
+  runBlockingMaybeCancellable {
+    val execPath = exec.toPath().toAbsolutePath().normalize()
+    val eelDescriptor = execPath.getEelDescriptor()
+    val eel = eelDescriptor.toEelApi()
 
-  val parentEnv =
-    if (passParentEnvironment) eel.exec.fetchLoginShellEnvVariables()
-    else emptyMap()
+    val parentEnv =
+      if (passParentEnvironment) eel.exec.fetchLoginShellEnvVariables()
+      else emptyMap()
 
-  val fullEnv = parentEnv + environment
+    val fullEnv = parentEnv + environment
 
-  val workingDirectoryPath = workingDirectory.toPath().asEelPath()
+    val workingDirectoryPath = workingDirectory.toPath().toAbsolutePath().normalize().asEelPath()
 
-  val eelProcess = eel.exec.spawnProcess(commands[0])
-    .args(commands.tail())
-    .workingDirectory(workingDirectoryPath)
-    .env(fullEnv)
-    .eelIt()
-  eelProcess.convertToJavaProcess()
-}
+    val eelProcess = eel.exec.spawnProcess(commands[0])
+      .args(commands.tail())
+      .workingDirectory(workingDirectoryPath)
+      .env(fullEnv)
+      .eelIt()
+    eelProcess.convertToJavaProcess()
+  }
